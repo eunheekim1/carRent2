27e115aa0285bbfe90fc261cb9ee7e2c265a7992
eunheekim1 <ehkim41@gmail.com>
2022-09-05 01:22:34 +0000

    test3

--- carMgmt/src/main/java/carrent/domain/CarMgmtRepository.java
+++ carMgmt/src/main/java/carrent/domain/CarMgmtRepository.java
@@ -3,7 +3,7 @@ package carrent.domain;
 import carrent.domain.*;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
-
+//
 @RepositoryRestResource(collectionResourceRel = "carMgmts", path = "carMgmts")
 public interface CarMgmtRepository
     extends PagingAndSortingRepository<CarMgmt, Long> {}
